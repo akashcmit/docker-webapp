061161c29ae89b93688b470ecd044bd63d2bfa10
Akash Nigam <akash.n@comakeit.com>
2017-12-04 18:35:17 +0530

    Infinispan | Added basic caching of student list

--- StudentApp/src/main/java/in/akash/dao/StudentDaoImpl.java
+++ StudentApp/src/main/java/in/akash/dao/StudentDaoImpl.java
@@ -10,15 +10,27 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
+
 import in.akash.db.entity.StudentEntity;
 
 @Stateless(name = "env/in.akash.dao.StudentDao")
 public class StudentDaoImpl implements StudentDao {
 
+	private final EmbeddedCacheManager cacheManager;
+	private Cache<String, List<StudentEntity>> cache;
+	private List<StudentEntity> studentList;
+
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
 
 	public StudentDaoImpl() {
+		cacheManager = new DefaultCacheManager();
+		cacheManager.defineConfiguration("student", new ConfigurationBuilder().build());
+		cache = cacheManager.getCache("student");
 	}
 
 	public void saveStudent(StudentEntity student) {
@@ -28,11 +40,16 @@ public class StudentDaoImpl implements StudentDao {
 	}
 
 	public List<StudentEntity> getAllStudents() {
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<StudentEntity> cq = cb.createQuery(StudentEntity.class);
-		Root<StudentEntity> from = cq.from(StudentEntity.class);
-		CriteriaQuery<StudentEntity> all = cq.select(from);
-		TypedQuery<StudentEntity> allQuery = em.createQuery(all);
-		return allQuery.getResultList();
+		studentList = (List<StudentEntity>) cache.get("studentList");
+		if (studentList == null) {
+			CriteriaBuilder cb = em.getCriteriaBuilder();
+			CriteriaQuery<StudentEntity> cq = cb.createQuery(StudentEntity.class);
+			Root<StudentEntity> from = cq.from(StudentEntity.class);
+			CriteriaQuery<StudentEntity> all = cq.select(from);
+			TypedQuery<StudentEntity> allQuery = em.createQuery(all);
+			studentList = allQuery.getResultList();
+			cache.put("studentList", studentList);
+		}
+		return studentList;
 	}
 }
