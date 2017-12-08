2dc1c770588ac36fff30b77bd43cf568873d3f6a
Akash Nigam <akash.n@comakeit.com>
2017-12-08 14:54:58 +0530

    Added global configuration for DIST_SYNC cache strategy

--- StudentApp/src/main/java/in/akash/dao/StudentDaoImpl.java
+++ StudentApp/src/main/java/in/akash/dao/StudentDaoImpl.java
@@ -11,7 +11,9 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
 import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 
@@ -19,7 +21,7 @@ import in.akash.db.entity.StudentEntity;
 
 @Stateless(name = "env/in.akash.dao.StudentDao")
 public class StudentDaoImpl implements StudentDao {
-
+	private static final String STUDENT_LIST = "studentList";
 	private final EmbeddedCacheManager cacheManager;
 	private Cache<String, List<StudentEntity>> cache;
 	private List<StudentEntity> studentList;
@@ -28,19 +30,29 @@ public class StudentDaoImpl implements StudentDao {
 	private EntityManager em;
 
 	public StudentDaoImpl() {
-		cacheManager = new DefaultCacheManager();
-		cacheManager.defineConfiguration("student", new ConfigurationBuilder().build());
+		GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
+	      global.transport().clusterName("StudentAppCluster");
+	      cacheManager = new DefaultCacheManager(global.build());
+	      ConfigurationBuilder config = new ConfigurationBuilder();
+	      config
+	          //.expiration().lifespan(10, TimeUnit.SECONDS)
+	          .clustering().cacheMode(CacheMode.DIST_SYNC);
+		
+		//cacheManager = new DefaultCacheManager();
+		//cacheManager.defineConfiguration("student", new ConfigurationBuilder().build());
+	    cacheManager.defineConfiguration("student", config.build());
 		cache = cacheManager.getCache("student");
 	}
 
 	public void saveStudent(StudentEntity student) {
 		em.persist(student);
 		System.out.println("New Student Saved : " + student.toString());
-
+		//reset cache
+		cache.clear();
 	}
 
 	public List<StudentEntity> getAllStudents() {
-		studentList = (List<StudentEntity>) cache.get("studentList");
+		studentList = (List<StudentEntity>) cache.get(STUDENT_LIST);
 		if (studentList == null) {
 			CriteriaBuilder cb = em.getCriteriaBuilder();
 			CriteriaQuery<StudentEntity> cq = cb.createQuery(StudentEntity.class);
@@ -48,7 +60,7 @@ public class StudentDaoImpl implements StudentDao {
 			CriteriaQuery<StudentEntity> all = cq.select(from);
 			TypedQuery<StudentEntity> allQuery = em.createQuery(all);
 			studentList = allQuery.getResultList();
-			cache.put("studentList", studentList);
+			cache.put(STUDENT_LIST, studentList);
 		}
 		return studentList;
 	}
